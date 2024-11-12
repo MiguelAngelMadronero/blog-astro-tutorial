a117f92b8cb7096567c84b8058496ef6800b3d7d
Miguel Madroñero <madronerosequeiramiguelangel@gmail.com>
2024-11-12 00:07:44 -0500

    terminado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://blog-astro-tutorial-mams.netlify.app/",
+  integrations: [preact()]
+});
